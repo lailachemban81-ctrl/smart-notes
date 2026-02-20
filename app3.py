a6e1477c074804a0371c2ef9ab3e02fdcd3aa10c
lailachemban81-ctrl <lailachemban81@gmail.com>
2026-02-20 23:51:56 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,3 +1,10 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+import os
+
+app = Flask(__name__)
+app.secret_key = "secret123"
+
 @app.route("/")
 def home():
-    return "APP WORKING PERFECT"
+    return "WORKING"
